308a3490f81bc150537abdf58230c37cbb7575f5
Daniël Illouz <daniel.illouz@me.com>
2019-05-27 21:53:41 +0200

    Use starter repo to setup a basic blog

---
+++ content/blog/hello-world/index.md
@@ -0,0 +1,19 @@
+---
+title: Hello World
+date: '2019-05-27T18:52:01.268Z'
+description: 'Stuff'
+---
+
+This is my first post on my new blog!
+
+I'll write more interesting things in the future. But first I'm trying to configure this monster.
+
+# How does code look like?
+
+```js
+function main() {
+  console.log('hello world);
+}
+
+main();
+```
---
+++ src/pages/404.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+function NotFoundPage({ data }) {
+  const siteTitle = data.site.siteMetadata.title;
+
+  return (
+    <Layout location={this.props.location} title={siteTitle}>
+      <SEO title="404: Not Found" />
+
+      <h1>Not Found</h1>
+
+      <p>Sorry, this page doesn&apos;t exist.</p>
+    </Layout>
+  );
+}
+
+export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
