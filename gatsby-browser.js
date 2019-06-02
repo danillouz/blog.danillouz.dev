b7f87d689168f6f481ba83c1da6e367b393d46f6
Daniël Illouz <daniel.illouz@me.com>
2019-06-02 15:09:23 +0200

    Add dark/light modes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@ import 'typeface-merriweather';
 
 // Custom styles
 import './src/css/global.css';
+import './src/css/theme-toggle.css';
 
 // Code syntax highlighting
 import './src/css/prism-night-owl.css';
